5d45034abc2b150acc08b62e8ca2a12195bb98d7
Xumidalu <135773159+Xumidalu@users.noreply.github.com>
2023-09-27 11:33:12 -0300

    sla

--- cod_resumido.py
+++ cod_resumido.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 
+# INICIANDO O JOGO & PREPARANDO O DIRETÓRIO DE IMAGENS
+
 pygame.init()
 
 x, y = 1600, 900
@@ -12,58 +14,52 @@ pygame.display.set_caption("PerfGens")
 
 main_dir = os.path.dirname(__file__)
 dir_img = os.path.join(main_dir, "cute_core")
-home = pygame.image.load(os.path.join(dir_img, "fundo_inicial.png")).convert_alpha()
-home = pygame.transform.scale(home, (1600, 900))
-start_ = pygame.image.load(os.path.join(dir_img, "iniciar.png")).convert_alpha()
-start_pressed = pygame.image.load(os.path.join(dir_img, "iniciar_pressionado.png")).convert_alpha()
-credits_ = pygame.image.load(os.path.join(dir_img, "créditos.png")).convert_alpha()
-credits_pressed = pygame.image.load(os.path.join(dir_img, "créditos_pressionado.png")).convert_alpha()
-exit_ = pygame.image.load(os.path.join(dir_img, "sair.png")).convert_alpha()
-exit_pressed = pygame.image.load(os.path.join(dir_img, "sair_pressionado.png")).convert_alpha()
-back = pygame.image.load(os.path.join(dir_img, "pink_back.png")).convert_alpha()
-back_pressed = pygame.image.load(os.path.join(dir_img, "yellow_back.png")).convert_alpha()
-continuar = pygame.image.load(os.path.join(dir_img, "continuar.png")).convert_alpha()
-continuar_pressed = pygame.image.load(os.path.join(dir_img, "continuar_amarelo.png")).convert_alpha()
-sala_das_camas = pygame.image.load(os.path.join(dir_img, "sala1_camas.png")).convert_alpha()
-sala_das_camas = pygame.transform.scale(sala_das_camas, (1600, 900))
-credits_background = pygame.image.load(os.path.join(dir_img, "fundo_dos_créditos.png")).convert_alpha()
-credits_background = pygame.transform.scale(credits_background, (1600, 900))
 
-salaTV = pygame.image.load(os.path.join(dir_img, "sala_da_tv.png")).convert_alpha()
-salaTV = pygame.transform.scale(salaTV, (1600, 900))
 
-black_square = pygame.image.load(os.path.join(dir_img, "balão_escuro.png")).convert_alpha()
-avante = pygame.image.load(os.path.join(dir_img, "mini_continuar.png")).convert_alpha()
-avante_hover = pygame.image.load(os.path.join(dir_img, "mini_continuar_hovering.png")).convert_alpha()
-cartas = pygame.image.load(os.path.join(dir_img, "carta.jpg")).convert_alpha()
-carta_funciona = pygame.transform.scale(cartas, (1600, 900))
-# SETAS CINZA
-seta = pygame.image.load(os.path.join(dir_img, "seta.png")).convert_alpha()
-seta = pygame.transform.scale(seta, (50, 50))
-seta_pressed = pygame.image.load(os.path.join(dir_img, "seta_pressed.png")).convert_alpha()
-seta_pressed = pygame.transform.scale(seta_pressed, (50, 50))
-seta_esquerda = pygame.image.load(os.path.join(dir_img, "seta_esquerda.png")).convert_alpha()
-seta_esquerda = pygame.transform.scale(seta_esquerda, (100, 100))
-seta_esquerda_pressed = pygame.image.load(os.path.join(dir_img, "seta_esquerda_pressed.png")).convert_alpha()
-seta_esquerda_pressed = pygame.transform.scale(seta_esquerda_pressed, (100, 100))
+# FUNÇÕES PARA IMAGENS
 
-seta_direita = pygame.image.load(os.path.join(dir_img, "seta cinza direita.png")).convert_alpha()
-seta_direita = pygame.transform.scale(seta_direita, (100, 100))
-seta_direita_pressed = pygame.image.load(os.path.join(dir_img, "seta cinza direita pressionada.png")).convert_alpha()
-seta_direita_pressed = pygame.transform.scale(seta_direita_pressed, (100, 100))
-#
-salaa2p1 = pygame.image.load(os.path.join(dir_img, "sala2pt1.png")).convert_alpha()
-sala2p1 = pygame.transform.scale(salaa2p1, (1600, 900))
-menu_middleb = pygame.image.load(os.path.join(dir_img, "FundinhoMiddleMenu.png")).convert_alpha()
-menu_middle = pygame.transform.scale(menu_middleb, (1600, 900))
-retomar = pygame.image.load(os.path.join(dir_img, "Menu_middle1.png")).convert_alpha()
-retomar_pressed = pygame.image.load(os.path.join(dir_img, "Menu_middle_pressed1.png")).convert_alpha()
-inventario = pygame.image.load(os.path.join(dir_img, "Menu_middle2.png")).convert_alpha()
-inventario_pressed = pygame.image.load(os.path.join(dir_img, "Menu_middle_pressed2.png")).convert_alpha()
-Menu_princ_volt = pygame.image.load(os.path.join(dir_img, "Menu_middle3.png")).convert_alpha()
-Menu_princ_volt_pressed = pygame.image.load(os.path.join(dir_img, "Menu_middle_pressed3.png")).convert_alpha()
-tela1_puzzle1 = pygame.image.load(os.path.join(dir_img, "puzzle1_recado1.png")).convert_alpha()
-tela1_puzzle1 = pygame.transform.scale(tela1_puzzle1, (1600, 900))
+
+def img_cut(img_name, in_x, in_y):
+    a = pygame.image.load(os.path.join(dir_img, img_name)).convert_alpha()
+    a = pygame.transform.scale(a, (in_x, in_y))
+    return a
+
+
+def img(img_name):
+    a = pygame.image.load(os.path.join(dir_img, img_name)).convert_alpha()
+    return a
+
+
+# IMPORTES DE IMAGEM DE FUNDO
+home = img_cut("fundo_inicial.png", 1600, 900)
+sala_das_camas = img_cut("sala1_camas.png", 1600, 900)
+credits_background = img_cut("fundo_dos_créditos.png", 1600, 900)
+salaTV = img_cut("sala_da_tv.png", 1600, 900)
+carta_funciona = img_cut("carta.jpg", 1600, 900)
+sala2p1 = img_cut("sala2pt1.png", 1600, 900)
+menu_middle = img_cut("FundinhoMiddleMenu.png", 1600, 900)
+tela1_puzzle1 = img_cut("puzzle1_recado1.png", 1600, 900)  # ATÉ ENTÃO SEM USO
+
+# IMPORTES DE IMAGENS DE ELEMENTOS
+start_, start_pressed = img("iniciar.png"), img("iniciar_pressionado.png")
+credits_, credits_pressed = img("créditos.png"), img("créditos_pressionado.png")
+exit_, exit_pressed = img("sair.png"), img("sair_pressionado.png")
+back, back_pressed = img("pink_back.png"), img("yellow_back.png")
+continuar, continuar_pressed = img("continuar.png"), img("continuar_amarelo.png")
+avante, avante_hover = img("mini_continuar.png"), img("mini_continuar_hovering.png")
+black_square = img("balão_escuro.png")
+retomar, retomar_pressed = img("Menu_middle1.png"), img("Menu_middle_pressed1.png")
+inventario, inventario_pressed = img("Menu_middle2.png"), img("Menu_middle_pressed2.png")
+middleToMain, middleToMainHover = img("Menu_middle3.png"), img("Menu_middle_pressed3.png")
+
+# IMPORTES DE IMAGENS DE ELEMENTOS / SETAS CINZA
+seta, seta_pressed = img_cut("seta.png", 50, 50), img_cut("seta_pressed.png", 50, 50)
+seta_left, seta_left_hover = img_cut("seta_esquerda.png", 100, 100), img_cut("seta_esquerda_pressed.png", 100, 100)
+seta_right, seta_right_hover = img_cut("seta cinza direita.png", 100, 100), img_cut(
+    "seta cinza direita pressionada.png", 100, 100)
+
+
+# IMPORTES AINDA NÃO ORGANIZADOS
 inventory_screen = pygame.image.load(os.path.join(dir_img, "inventário.png")).convert_alpha()
 inventory_screen = pygame.transform.scale(inventory_screen, (1600, 900))
 loucuras = pygame.image.load(os.path.join(dir_img, "importLoUcUrAs.png")).convert_alpha()
@@ -73,7 +69,7 @@ pontos = 0
 estante = pygame.image.load("Estante/Estanteampli.png")
 estanteR = pygame.transform.scale(estante, (1600, 900))
 caixa = pygame.image.load("Estante/caixa.png")
-caixaR = pygame.transform.scale(caixa,(200, 200))
+caixaR = pygame.transform.scale(caixa, (200, 200))
 caixapressed = pygame.image.load("Estante/caixapressed.png")
 caixapressedR = pygame.transform.scale(caixapressed, (200, 200))
 pastaroxa = pygame.image.load("Estante/pastaroxa.png")
@@ -146,13 +142,10 @@ chip = pygame.transform.scale(chip, (58, 40))
 
 chipBIG = pygame.transform.scale(chip, (174, 120))
 
-
 pau = pygame.image.load(os.path.join(dir_img, "pau.png")).convert_alpha()
 pau = pygame.transform.scale(pau, (130, 7))
 
 pauBIG = pygame.transform.scale(pau, (390, 21))
-
-
 
 telaquizza = pygame.image.load("quiz/Telaquiz.png")
 telaquiz = pygame.transform.scale(telaquizza, (1600, 900))
@@ -167,7 +160,7 @@ K = pygame.image.load("quiz/K.png")
 
 Na = pygame.image.load("quiz/Na.png")
 
-Cl =pygame.image.load("quiz/Cl.png")
+Cl = pygame.image.load("quiz/Cl.png")
 
 Q2 = pygame.image.load("quiz/Q2.png")
 
@@ -187,12 +180,12 @@ MgCl2 = pygame.image.load("quiz/MgCl2.png")
 
 MDC = pygame.image.load("quiz/MDC.png")
 
-
 tv_on = pygame.image.load(os.path.join(dir_img, "tv_ligada.png")).convert_alpha()
 tv_on = pygame.transform.scale(tv_on, (470, 300))
 
 tv_off = pygame.image.load(os.path.join(dir_img, "tv_apagada.png")).convert_alpha()
 tv_off = pygame.transform.scale(tv_off, (470, 300))
+
 
 def recadinhos(var, color):
     return fonte.render(var, True, color)
@@ -280,7 +273,7 @@ class SalaDasCamas:
         window.blit(black_square, (100, 100))
         self.avante = Button(1350, 285, avante, avante_hover)
         self.ver_a_carta = Button(1200, 560, seta, seta_pressed)
-        self.ir_pra_sala_esquerda = Button(100, 560, seta_esquerda, seta_esquerda_pressed)
+        self.ir_pra_sala_esquerda = Button(100, 560, seta_left, seta_left_hover)
         self.buraco = Button(728, 385, buraco, buraco)
         self.hidden_button = Button(730, 380, hidden_button, hidden_button)
         self.recado = recadinhos(texto1, WHITE)
@@ -376,7 +369,7 @@ class SalaDasCamas:
 class Carta:
     def __init__(self):
         window.blit(carta_funciona, (0, 0))
-        self.voltar = Button(100, 500, seta_esquerda, seta_esquerda_pressed)
+        self.voltar = Button(100, 500, seta_left, seta_left_hover)
 
     def handle_events(self):
         global history
@@ -402,14 +395,14 @@ class Carta:
 class PuzzleSalaUmParaDois:
     def __init__(self):
         window.blit(tela1_puzzle1, (0, 0))
-        self.ir_pra_sala_esquerda = Button(100, 560, seta_esquerda, seta_esquerda_pressed)
+        self.ir_pra_sala_esquerda = Button(100, 560, seta_left, seta_left_hover)
 
 
 class SalaDaTV:
     def __init__(self):
         window.blit(salaTV, (0, 0))
-        self.ir_pra_sala_esquerda = Button(100, 560, seta_esquerda, seta_esquerda_pressed)
-        self.voltar_pra_garrafona1 = Button(1500, 560, seta_direita, seta_direita_pressed)
+        self.ir_pra_sala_esquerda = Button(100, 560, seta_left, seta_left_hover)
+        self.voltar_pra_garrafona1 = Button(1500, 560, seta_right, seta_right_hover)
         self.tv_off = Button(565, 210, tv_off, tv_off)
         self.tv_on = Button(565, 210, tv_on, tv_on)
         self.varControle = 0
@@ -481,7 +474,7 @@ class SalaDaTV:
 class Sala_das_Garrafa_pt_2:
     def __init__(self):
         window.blit(sala2pt2, (0, 0))
-        self.voltar_pra_tv = Button(1500, 560, seta_direita, seta_direita_pressed)
+        self.voltar_pra_tv = Button(1500, 560, seta_right, seta_right_hover)
 
     def handle_events(self):
         global history
@@ -514,7 +507,7 @@ class SaladasGarrafonas:
         self.escada = Button(680, 300, seta, seta_pressed)
         self.avante = Button(1350, 285, avante, avante_hover)
         self.loucuras = Button(1100, 336, loucuras, loucuras)
-        self.ir_pra_sala_esquerda = Button(100, 560, seta_esquerda, seta_esquerda_pressed)
+        self.ir_pra_sala_esquerda = Button(100, 560, seta_left, seta_left_hover)
         self.caixa = black_square
         self.texto = texto5
         self.texto2 = texto8
@@ -524,7 +517,7 @@ class SaladasGarrafonas:
         self.recadin2 = recadinhos(self.texto2, WHITE)
         self.recadin3 = recadinhos(self.texto10, WHITE)
         self.recadin4 = recadinhos(self.texto11, WHITE)
-        self.voltar_pra_sala_das_camas = Button(1500, 560, seta_direita, seta_direita_pressed)
+        self.voltar_pra_sala_das_camas = Button(1500, 560, seta_right, seta_right_hover)
         window.blit(self.caixa, (100, 100))
         window.blit(self.recadin, (200, 200))
         self.msg = False
@@ -545,7 +538,6 @@ class SaladasGarrafonas:
         self.chip_e_pau_on = False
         self.pau_on = False
         self.chip_on = False
-
 
     def handle_events(self):
         global history
@@ -584,7 +576,6 @@ class SaladasGarrafonas:
                 self.chip.bye_bye()
                 self.contagem_do_black_square_pt4 += 1
 
-
         if self.escada.check_click():
             self.msg = True
         if self.avante.check_click():
@@ -603,7 +594,6 @@ class SaladasGarrafonas:
                 print(e)
 
         return "sala2p1"
-
 
     def update(self):
         global inventory_lot_1, inventory_lot_2, inventory_lot_3
@@ -645,7 +635,7 @@ class SaladasGarrafonas:
             self.show_potion = False
             inventory_lot_1 = "Loucuras"
 
-# BLACK_SQUARE DA POÇÃO
+        # BLACK_SQUARE DA POÇÃO
         if self.contagem_do_black_square_pt2 > 2:
             window.blit(black_square, (100, 550))
             window.blit(self.recadin2, (200, 645))
@@ -654,7 +644,7 @@ class SaladasGarrafonas:
             self.contagem_do_black_square_pt2 = 0
             self.varControle = 0
 
-# BLACK_SQUARE DO PAU
+        # BLACK_SQUARE DO PAU
         if self.contagem_do_black_square_pt3 > 2:
             window.blit(black_square, (100, 550))
             window.blit(self.recadin3, (200, 645))
@@ -663,7 +653,7 @@ class SaladasGarrafonas:
             self.contagem_do_black_square_pt3 = 0
             self.varControle2 = 0
 
-# BLACK_SQUARE DO CHIP
+        # BLACK_SQUARE DO CHIP
         if self.contagem_do_black_square_pt4 > 2:
             window.blit(black_square, (100, 550))
             window.blit(self.recadin4, (200, 645))
@@ -695,7 +685,7 @@ class Estante:
         self.caixa = Button(840, 750, caixaR, caixapressedR)
         self.pastaroxa = Button(680, 225, pastaroxaR, pastaroxapressedR)
         self.pastaverde = Button(870, 225, pastaverdeR, pastaverdepressedR)
-        self.volta = Button(300, 300, seta_esquerda, seta_esquerda_pressed)
+        self.volta = Button(300, 300, seta_left, seta_left_hover)
         self.Ca = False
         self.Pr = False
         self.Pv = False
@@ -713,7 +703,6 @@ class Estante:
             self.princ = False
         elif name == False:
             pass
-
 
     def handle_events(self):
         global history
@@ -791,7 +780,7 @@ class Inventory:
     def __init__(self):
         global inventory_lot_1
         window.blit(inventory_screen, (0, 0))
-        self.close_inventory = Button(100, 800, seta_esquerda, seta_esquerda_pressed)
+        self.close_inventory = Button(100, 800, seta_left, seta_left_hover)
         self.seta_esquerda = Button(250, 500, seta_preta_esquerda, seta_preta_esquerda_pressionada)
         self.seta_direita = Button(1350, 500, seta_preta_direita, seta_preta_direita_pressionada)
         self.loucuras = Button(800, 450, loucurasBIG, loucurasBIG)
@@ -897,7 +886,6 @@ class Quizzz():
         self.Q3 = False
         self.resp = False
 
-
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -914,7 +902,7 @@ class Quizzz():
                 if self.Cl.check_click():
                     self.Q1 = False
                     self.Q2 = True
-                    self.comc =True
+                    self.comc = True
                 elif self.K.check_click() or self.Na.check_click():
                     return "salaTV"
             elif self.Q2 == True:
@@ -936,7 +924,7 @@ class Quizzz():
         return "quiz"
 
     def update(self):
-        window.blit(telaquiz, (0,0))
+        window.blit(telaquiz, (0, 0))
         if self.inicio == True:
             self.Inic.draw()
         elif self.comc == True:
@@ -965,14 +953,13 @@ class Quizzz():
             window.blit(MDC, (0, 0))
 
 
-
 class MiddleMenu:
     def __init__(self):
         global history
         window.blit(menu_middle, (0, 0))
         self.retomar = Button(790, 255, retomar, retomar_pressed)
         self.invent = Button(790, 455, inventario, inventario_pressed)
-        self.menuinic = Button(790, 655, Menu_princ_volt, Menu_princ_volt_pressed)
+        self.menuinic = Button(790, 655, middleToMain, middleToMainHover)
         self.varControle = 0
 
     def handle_events(self):
